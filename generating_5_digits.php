1a589daa6bb1fa18321320e4f5bf869845dd3439
Dom McCarty, PhD <dominic.mccarty@gmail.com>
2017-01-18 19:44:45 -0800

    PSR2 syntax

--- generating_5_digits.php
+++ generating_5_digits.php
@@ -9,40 +9,39 @@
  * use the prime of your choice. we use 413158523
  */
 
-require("base_helpers.php");
+require "base_helpers.php";
 
 function code_algorithm()
 {
 
     $counter_filename = "codes_counter.txt"; //initialize it with "0"
-	
-	if (!file_exists($counter_filename)) {
-		
-		file_put_contents($counter_filename, "0");
-	}
+
+    if (!file_exists($counter_filename)) {
+        file_put_contents($counter_filename, "0");
+    }
 
     $fp = fopen($counter_filename, "r+");
 
     if (flock($fp, LOCK_EX)) {  // acquire an exclusive lock. blocks until it succeeds.
         
         $counter = (int) trim(fgets($fp));
-    
+
         $modulus = 916132832; // = 62^5
-    
+
         $prime = 413158523;
-    
-    
+
+
         $next = ((++$counter) * $prime) % $modulus;
-    
+
         $next = to_5_char_base_62($next);
-    
-        
+
+
         ftruncate($fp, 0);
-        
+
         fwrite($fp, $counter);
-        
+
         fflush($fp);            // flush output before releasing the lock
-        
+
         flock($fp, LOCK_UN);    // release the lock
 
         return $next;
